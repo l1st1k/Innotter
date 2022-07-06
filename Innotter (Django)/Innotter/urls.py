ed8c4650ac90e1ba358cbd66dcad8a273ec2045a
l1st1k <listik.mp@gmail.com>
2022-07-06 15:32:49 +0300

    changed architecture

--- Innotter (Django)/Innotter/urls.py
+++ Innotter (Django)/Innotter/urls.py
@@ -1,4 +1,4 @@
-"""Innotter URL Configuration
+"""Innotter (Django) URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
