63446a8a764e994c44461e33eb16c9d7290fbe85
l1st1k <listik.mp@gmail.com>
2022-08-01 14:00:09 +0300

    Fixes for pr #8 (microservice)

--- Service_FastAPI/main.py
+++ Service_FastAPI/main.py
@@ -2,21 +2,20 @@ import requests
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
 
-
 app = FastAPI()
 
 
 class UserModel(BaseModel):
-    username: str = Field(title='Логин', max_length=30)
-    password: str = Field(title='Пароль', max_length=30)
+    username: str = Field(title='Login', max_length=30)
+    password: str = Field(title='Password', max_length=30)
 
 
 class StatsResponseModel(BaseModel):
-    page_name: str = Field(title='Название страницы', max_length=80)
-    page_id: int = Field(title='ID страницы', ge=0)
-    amount_of_posts: int = Field(title='Количество постов', ge=0)
-    amount_of_followers: int = Field(title='Количество подписчиков', ge=0)
-    amount_of_likes: int = Field(title='Количество лайков', ge=0)
+    page_name: str = Field(title='Page name', max_length=80)
+    page_id: int = Field(title='Page ID', ge=0)
+    amount_of_posts: int = Field(title='Amount of posts', ge=0)
+    amount_of_followers: int = Field(title='Amount of followers', ge=0)
+    amount_of_likes: int = Field(title='Amount of likes', ge=0)
 
 
 def get_tokens_for_cookies(login, password):
@@ -36,18 +35,29 @@ def get_amount_of_likes(posts) -> int:
     return counter
 
 
-def request_to_django(page_id: int, user_login: str, user_pass: str) -> dict:
-    cookies = get_tokens_for_cookies(user_login, user_pass)
+def get_request_to_django_page(page_id: int, cookies: dict, url: str):
     headers = {"Authorization": f"Bearer {cookies['access_token']}", "Content-Type": "application/json"}
-    response_name = requests.get(f"http://localhost:8000/api/v1/page/{page_id}/", headers=headers, cookies=cookies)
+    url = url.replace('{page_id}', str(page_id))
+    response = requests.get(url=url, headers=headers, cookies=cookies)
+    return response
+
+
+@app.post("/api/v1/page/{page_id}/stats", response_model=StatsResponseModel, name='Page stats by ID')
+async def stats(page_id: int, user: UserModel):
+    cookies = get_tokens_for_cookies(user.username, user.password)
+    response_name = get_request_to_django_page(page_id=page_id, cookies=cookies,
+                                               url="http://localhost:8000/api/v1/page/{page_id}/")
     page_name = response_name.json()["name"]
-    response_followers = requests.get(f"http://localhost:8000/api/v1/page/{page_id}/followers/", headers=headers,
-                                      cookies=cookies)
+
+    response_followers = get_request_to_django_page(page_id=page_id, cookies=cookies,
+                                                    url="http://localhost:8000/api/v1/page/{page_id}/followers/")
     amount_of_followers = len(response_followers.json()['followers'])
-    response_posts = requests.get(f"http://localhost:8000/api/v1/page/{page_id}/posts/", headers=headers,
-                                  cookies=cookies)
+
+    response_posts = get_request_to_django_page(page_id=page_id, cookies=cookies,
+                                                url="http://localhost:8000/api/v1/page/{page_id}/posts/")
     amount_of_posts = len(response_posts.json()['posts'])
     amount_of_likes = get_amount_of_likes(response_posts.json()['posts'])
+
     response = {
         'page_name': page_name,
         'page_id': page_id,
@@ -56,8 +66,3 @@ def request_to_django(page_id: int, user_login: str, user_pass: str) -> dict:
         'amount_of_likes': amount_of_likes
     }
     return response
-
-
-@app.post("/api/v1/page/{page_id}/stats", response_model=StatsResponseModel, name='Page stats by ID')
-async def stats(page_id: int, user: UserModel):
-    return request_to_django(page_id, user.username, user.password)
